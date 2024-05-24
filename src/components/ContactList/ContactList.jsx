3faf1ea8946f770b838b4485eea7c13a627a804e
AnnaLvchnk <nlvcnk@gmail.com>
2024-05-24 16:25:22 +0200

    changed contactlist

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,20 @@
 import { useSelector } from 'react-redux';
 import { selectVisibleContacts } from '../../redux/contacts/selectors';
 import Contact from "../Contact/Contact";
-import { List } from '@mui/material';
+import css from './ContactList.module.css';
 
-export default function ContactList() {
+const ContactList = () => {
 
   const contacts = useSelector(selectVisibleContacts);
 
   return (
-    Array.isArray(contacts) &&
-    <List sx={{display:"flex", justifyContent:"space-around", flexWrap:"wrap", gap:"12px", mt:2}}>
+    <ul className={css.contactList}>
         {contacts.map((contact) => {
             return <Contact key={contact.id} contact={contact} />
         })}
-    </List>
+    </ul>
   )
 
 }
+
+export default ContactList;
